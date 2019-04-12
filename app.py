2fd4e6aad706b37bb755a01c8d0ecc112edfae27
Pili Mayora <pili.mayora@gmail.com>
2019-04-12 19:23:39 +0100

    Adding one more route to try Apache redirect

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hi International SuperFriends. We have a website running!'
+
+
+@app.route('/genre/<genre>')
+def show_genre(genre):
+    return 'Genre %s' % genre
