44680c32c1fff1058f17dd0cbde5c1a26b99fa15
nottuswehttam <44438139+nottuswehttam@users.noreply.github.com>
2019-04-30 19:58:23 +0100

    Update ticketmaster-API.py
    Additions to allow pagination scraping:
    1) commented out all states other than NY, since this is the one giving us problems
    2) added a second apikey in case the first reaches its limit; commented out
    3) added page size parameter var "size"
    4) created a pages_list that is 0 if there there is only one page, and 0 to pages_num - 1, otherwise
    5) implemented a for-loop to iterate through each page in pages_list
    6) added two try-except procedures to gracefully catch when ['name'] and ['classification'] keys are not in the 'artists' dict; both fields are set to empty strings ('') if nonexistent
    7) added an entry var capturing how many entries have been added to the db.
    8) after each entry is added to the db a message prints which page of page_list the for-loop is on, the number of the entry added, and the artist_name of that entry

--- scripts/ticketmaster-API.py
+++ scripts/ticketmaster-API.py
@@ -17,63 +17,96 @@ cursor = db.cursor()
 
 URL = "https://app.ticketmaster.com/discovery/v2/events.json"
 
-us_states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE",
-             "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
-             "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS",
-             "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY",
-             "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-             "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+us_states = [#"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE",
+             #"FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
+             #"KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS",
+             #"MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY",
+             #"NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+             #"SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI",
+             "NY"]
 
 apikey = "sDeUMZRAoiaKBkELqJqSCS2phLMxJj4A"
+#apikey2 = "s10q0X1HOmg4rgAtNS5wuhgekEcFaGYC"
 classificationName = "music"
+size = 200
 
 for us_state in us_states:
     params = {'stateCode': us_state,
               'classificationName': classificationName,
               'apikey': apikey,
-              'startEndDateTime': ['2018-01-01T00:00:00Z',
-                                   '2018-12-31T23:59:59Z']}
+              'startEndDateTime': ['2019-05-01T00:00:00Z',
+                                   '2019-05-31T23:59:59Z'],
+              'size': size}
 
     r = requests.get(url=URL, params=params)
 
     data = r.json()
 
-    events = data['_embedded']['events']
+    pages_num = data['page']['totalPages']
+    if pages_num == 1:
+        pages_list = [0]
+    else:
+        pages_list = range(0,pages_num-1)
 
-    for index, event in enumerate(events):
-        local_date = event['dates']['start']['localDate']
-        event_id = event['id']
+    #print(us_state)
+    #print(pages_num)
 
-        main_genre = event['classifications'][0].get('genre')
-        main_genre_name = main_genre['name'] if main_genre else ''
-        sub_genre = event['classifications'][0].get('subGenre')
-        sub_genre_name = sub_genre['name'] if sub_genre else ''
+    entry = 1
 
-        main_venue = event['_embedded']['venues'][0]
-        main_venue_name = main_venue['name']
+    for page in pages_list:
+        params = {'stateCode': us_state,
+                  'classificationName': classificationName,
+                  'apikey': apikey,
+                  'startEndDateTime': ['2019-05-01T00:00:00Z',
+                                       '2019-05-31T23:59:59Z'],
+                  'size': size,
+                  'page': page
+                  }
 
-        main_venue_location = main_venue.get('location')
-        main_venue_lat = main_venue['location'][
-            'latitude'] if main_venue_location else None
-        main_venue_lon = main_venue['location'][
-            'longitude'] if main_venue_location else None
+        r = requests.get(url=URL, params=params)
 
-        artists = event['_embedded'].get('attractions')
-        main_artist_id = artists[0]['id'] if artists else ''
-        main_artist_name = artists[0]['name'] if artists else ''
-        main_artist_genre = artists[0]['classifications'][
-            0]['genre']['name'] if artists else ''
+        data = r.json()
 
-        query = """INSERT INTO ticketmaster_events
-                    (ticketmaster_id, local_date, event_genre,
-                    event_subgenre, venue, venue_lat, venue_long,
-                    artist_id, artist_name, artist_genre) VALUES
-                    (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-        values = (event_id, local_date, main_genre_name, sub_genre_name,
-                  main_venue_name, main_venue_lat, main_venue_lon,
-                  main_artist_id, main_artist_name, main_artist_genre)
+        events = data['_embedded']['events']
 
-        cursor.execute(query, values)
-        db.commit()
+        #print(us_state,page)
 
-    time.sleep(1)
+        for index, event in enumerate(events):
+            local_date = event['dates']['start']['localDate']
+            event_id = event['id']
+
+            main_genre = event['classifications'][0].get('genre')
+            main_genre_name = main_genre['name'] if main_genre else ''
+            sub_genre = event['classifications'][0].get('subGenre')
+            sub_genre_name = sub_genre['name'] if sub_genre else ''
+
+            main_venue = event['_embedded']['venues'][0]
+            main_venue_name = main_venue['name']
+
+            main_venue_location = main_venue.get('location')
+            main_venue_lat = main_venue['location'][
+                'latitude'] if main_venue_location else None
+            main_venue_lon = main_venue['location'][
+                'longitude'] if main_venue_location else None
+
+            artists = event['_embedded'].get('attractions')
+            main_artist_id = artists[0]['id'] if artists else ''
+            try:main_artist_name = artists[0]['name'] if artists else ''
+            except: main_artist_name = ''
+            try: main_artist_genre = artists[0]['classifications'][0]['genre']['name'] if artists else ''
+            except: main_artist_genre = ''
+
+            query = """INSERT INTO ticketmaster_events
+                        (ticketmaster_id, local_date, event_genre,
+                        event_subgenre, venue, venue_lat, venue_long,
+                        artist_id, artist_name, artist_genre) VALUES
+                        (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            values = (event_id, local_date, main_genre_name, sub_genre_name,
+                      main_venue_name, main_venue_lat, main_venue_lon,
+                      main_artist_id, main_artist_name, main_artist_genre)
+
+            cursor.execute(query, values)
+            db.commit()
+            print("page",page,"   ","entry",entry,"   ","artist",main_artist_name)
+            entry = entry + 1
+        time.sleep(1)
