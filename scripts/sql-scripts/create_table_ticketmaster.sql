b1380583a2aab4c0243baf64f111d46eac65e8f3
Pili Mayora <pili.mayora@gmail.com>
2019-05-03 11:35:08 +0200

    Adding create statement for ticketmaster table

---
+++ scripts/sql-scripts/create_table_ticketmaster.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `state_of_music`.`ticketmaster_events` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `ticketmaster_id` VARCHAR(100) NOT NULL,
+  `local_date` DATE NULL,
+  `event_genre` VARCHAR(50) NULL,
+  `event_subgenre` VARCHAR(50) NULL,
+  `venue` VARCHAR(100) NULL,
+  `venue_lat` DECIMAL(10,8) NULL,
+  `venue_long` DECIMAL(11,8) NULL,
+  `artist_id` VARCHAR(50) NULL,
+  `artist_name` VARCHAR(100) NULL,
+  `artist_genre` VARCHAR(50) NULL,
+  PRIMARY KEY (`id`));
