3528cd9ca18f9f44551bb7a49b67ebd5a46d3cf2
richa-sud <richasud@gmail.com>
2019-05-09 16:28:48 +0100

    fixing indentation

--- static/js/colors_genres.js
+++ static/js/colors_genres.js
@@ -1,23 +1,23 @@
-  const genres = ['pop', 'rock_metal', 'indie', 'hiphop', 'rnb', 'classical_jazz', 'electronic_dance', 'country_folk'];
+const genres = ['pop', 'rock_metal', 'indie', 'hiphop', 'rnb', 'classical_jazz', 'electronic_dance', 'country_folk'];
 
-  const colors = {
-    pop: "#002f81",
-    rock_metal: "#2a7187",
-    indie: "#0381b4",
-    hiphop: "#4a2777",
-    rnb: "#b41162",
-    classical_jazz: "#bf9076",
-    electronic_dance: "#56371b",
-    country_folk: "#545a66",
-  }
+const colors = {
+  pop: "#002f81",
+  rock_metal: "#2a7187",
+  indie: "#0381b4",
+  hiphop: "#4a2777",
+  rnb: "#b41162",
+  classical_jazz: "#bf9076",
+  electronic_dance: "#56371b",
+  country_folk: "#545a66",
+}
 
-  const tool_txt = {
-    pop: "Pop",
-    rock_metal: "Rock & Metal",
-    indie: "Indie",
-    hiphop: "Hip Hop",
-    rnb: "R&B",
-    classical_jazz: "Classical & Jazz",
-    electronic_dance: "Electronic & Dance",
-    country_folk: "Country & Folk"
-  }
+const tool_txt = {
+  pop: "Pop",
+  rock_metal: "Rock & Metal",
+  indie: "Indie",
+  hiphop: "Hip Hop",
+  rnb: "R&B",
+  classical_jazz: "Classical & Jazz",
+  electronic_dance: "Electronic & Dance",
+  country_folk: "Country & Folk"
+}
