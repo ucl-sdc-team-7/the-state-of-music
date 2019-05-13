32bd9252425798d9c5dd474972259ad066e1412a
Cleo Au <43819433+cleoau@users.noreply.github.com>
2019-05-13 08:16:15 +0100

    Update wiki.py

--- genre/wiki.py
+++ genre/wiki.py
@@ -6,18 +6,6 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-#for id in soup.find_all('Genres'):
-#if "Genres[edit]" in soup.find_all('h2'):
-    #for sub_heading in soup.find_all('h3'):
 for link in soup.find_all('a'):
         #print(sub_heading.text)
     print(link.get_text('title'))
-
-
-
-
-# to fetch all <li> tas
-
-#for link in soup.find_all(attrs={"class" : "mw-headline"}):
-
-        #print(link.get_text('title'))
