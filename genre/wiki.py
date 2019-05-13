ff8e1f248e32bf63de93a9588f9ea6a8fb965916
Cleo Au <43819433+cleoau@users.noreply.github.com>
2019-05-13 07:51:03 +0100

    Wikipedia scraping for genre
    Cleo's really bad soup for music genre on Wikipedia (there are a lot of duplication and it does not have any Classical catergories).

---
+++ genre/wiki.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/List_of_popular_music_genres"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#for id in soup.find_all('Genres'):
+#if "Genres[edit]" in soup.find_all('h2'):
+    #for sub_heading in soup.find_all('h3'):
+for link in soup.find_all('a'):
+        #print(sub_heading.text)
+    print(link.get_text('title'))
+
+
+
+
+# to fetch all <li> tas
+
+#for link in soup.find_all(attrs={"class" : "mw-headline"}):
+
+        #print(link.get_text('title'))
