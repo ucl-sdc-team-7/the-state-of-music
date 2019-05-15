455c03ba1baaca9096ef35e482981ed0d905855f
richa-sud <richasud@gmail.com>
2019-05-15 23:46:12 +0100

    version 1 of city map

--- static/js/usCounties.js
+++ static/js/usCounties.js
@@ -26,7 +26,7 @@ usCounties.draw = function(bbox, genre) {
 
   function style() {
     return {
-      color: "#ddd", // border color
+      color: "#fff", // border color
       weight: 1,
       fillOpacity: 0.7,
     }
@@ -46,22 +46,61 @@ usCounties.draw = function(bbox, genre) {
     layer.closePopup()
   }
 
+  function cityMap(color){
+
+    function marker_colors(d){
+      return d > 0.9 ? 0.9 :
+           d > 0.8  ? 0.8 :
+           d > 0.7  ? 0.7 :
+           d > 0.6  ? 0.6 :
+           d > 0.5   ? 0.5 :
+           d > 0.4   ? 0.4 :
+           d > 0.4   ? 0.3 :
+                      0.2;
+    }
+
+    function onEachFeature_city(feature, layer) {
+      var popupCity = "<h4>" + feature.properties.address + "</h4>" +
+      "<table><tr><td>R&B</td><td>" + feature.properties.value * 100 + "%</td></tr>" +
+      "</table>" +
+      "<small>(click to zoom)</small>"
+
+    layer.bindPopup(popupCity, {
+        'className': 'custom'
+      }, );
+    }
+
+    var venues = L.geoJson(venue_geo, {
+      pointToLayer: function (feature, latlng) {
+        var geojsonMarkerOptions = {
+          radius: 10,
+          fillColor: color,
+          weight: 0,
+          fillOpacity: marker_colors(feature.properties.value),
+        };
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+      },
+      onEachFeature: onEachFeature_city
+    }).addTo(countyMap);
+  };
+
   function zoomToFeature(e) {
     window.choropleth.remove()
     countyMap.fitBounds(e.target.getBounds());
     //add base map to div
     basemap.addTo(countyMap);
 
+    return cityMap("#4a2777")
   }
 
-  function onEachFeature(feature, layer) {
+  function onEachFeature_county(feature, layer) {
 
-    var popupContent = "<h4>" + feature.properties.NAME + " County</h4>" +
+    var popupCounty = "<h4>" + feature.properties.NAME + " County</h4>" +
       "<table><tr><td>R&B</td><td>" + feature.properties.value * 100 + "%</td></tr>" +
       "</table>" +
       "<small>(click to zoom)</small>"
 
-    layer.bindPopup(popupContent, {
+    layer.bindPopup(popupCounty, {
       closeButton: false,
       'className': 'custom'
     }, );
@@ -79,7 +118,7 @@ usCounties.draw = function(bbox, genre) {
     steps: 10,
     mode: "q", // q for quantile
     style: style,
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature_county
   }).addTo(countyMap);
 
 
