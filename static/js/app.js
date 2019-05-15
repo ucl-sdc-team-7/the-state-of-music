d2f4310e016107d3948f25be5ce643ae633548c6
Will Low <44437051+williamlow@users.noreply.github.com>
2019-05-15 01:14:34 +0100

    Changes to info text box
    - Clicking the genres changes the text
    - Clicking the main icon resets to default text
    - The colour of the info text box also updates, matching the genre colour

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,31 @@
 uStates.draw("topgenre")
 stats.draw("topgenre")
+
+function updateInfoBox(label,color){
+		$("#titleGenre").html(label);
+		document.documentElement.style.setProperty('--genre-color', color);
+		$("#titleCategory").html("Where live "+label+" music is most popular")
+	}
+
 //Update data section (Called from the onclick)
 function updateData() {
-  d3.selectAll(".genre-icon")
-    .on('click', function() {
-      var genre_id = d3.select(this).attr('id')
-      d3.selectAll("svg > *").remove();
-      uStates.draw(genre_id);
-      stats.draw(genre_id);
+	d3.selectAll(".genre-icon")
+		.on('click', function() {
+			var genre_id = d3.select(this).attr('id')
+			d3.selectAll("svg > *").remove();
+			uStates.draw(genre_id);
+			stats.draw(genre_id);
+			updateInfoBox(GENRES[genre_id].label,GENRES[genre_id].color);
     });
 }
 function updatelogo() {
-  d3.selectAll('#logo')
-    .on('click', function() {
-      d3.selectAll("svg > *").remove();
-      uStates.draw("topgenre");
-      stats.draw("topgenre")
+	d3.selectAll('#logo')
+		.on('click', function() {
+			d3.selectAll("svg > *").remove();
+			uStates.draw("topgenre");
+			stats.draw("topgenre")
+			$("#titleGenre").html("music");
+			document.documentElement.style.setProperty('--genre-color', "#888b94");
+			$("#titleCategory").html("The most popular types of live music")
     });
 }
