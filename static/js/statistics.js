0de66e63acbc0ba00bd4dca12da3d64079efdc14
richa-sud <richasud@gmail.com>
2019-05-23 11:42:05 +0100

    working stats using rankings

--- static/js/statistics.js
+++ static/js/statistics.js
@@ -3,7 +3,7 @@ const bar_margin = {
     top: 20,
     right: 20,
     bottom: 20,
-    left: 60
+    left: 150
   },
   bar_width = 300 - bar_margin.left - bar_margin.right,
   bar_height = 300 - bar_margin.top - bar_margin.bottom;
@@ -15,15 +15,15 @@ const stats = {};
 
 stats.draw = function(genre) {
 
-  console.log(geo_level)
-
   const params = jQuery.param({
     genre: genre,
     level: geo_level
   });
+
   var request_url = "genre?" + params;
   d3.json(request_url, function(error, data) {
     if (error) console.log(error);
+
     data = data['data']
 
     //sort bars based on value
@@ -36,17 +36,6 @@ stats.draw = function(genre) {
     //finding top five states
     data = data.slice(-5, max_rank.ranking)
 
-    // setting domains for bars
-    x.domain([0, d3.max(data, function(d) {
-      return d.ranking
-    })]);
-    y.domain(data.map(function(d) {
-      return d.state_name
-    })).padding(0.3);
-
-    //creating y-axis
-    const yAxis = d3.axisLeft(y);
-
     //creating svg object
     const chart = d3.select("#displayStats").select("svg")
 
@@ -54,43 +43,94 @@ stats.draw = function(genre) {
     const bar = chart.append("g")
       .attr("transform", "translate(" + bar_margin.left + "," + bar_margin.top + ")")
 
+    //creating y-axis
+    const yAxis = d3.axisLeft(y);
+
     if (genre != "top") {
 
-      //assigning y-axis
-      bar.append('g').attr("class", "y axis").call(d3.axisLeft(y))
+      // setting domains for bars
+      x.domain([0, d3.max(data, function(d) {
+        return d.ranking
+      })]);
 
       //drawing bars in chart
       bar.selectAll('.bar').data(data)
         .enter().append('rect')
         .attr("class", "bar")
-        .attr('y', function(d) {
-          return y(d.state_name);
-        }) //assigning hieght of bars
         .attr("width", function(d) {
           return x(d.ranking);
         }) //assigning width of bars
-        .attr("height", y.bandwidth())
         .attr("fill", function(d) {
           return GENRES[genre].color
         })
 
-      //adding values to bars
-      bar.selectAll(".text")
-        .data(data).enter()
-        .append("text")
-        .attr("class", "bar-text")
-        .attr("dy", ".35em")
-        .attr("x", function(d) {
-          return x(d.ranking) - 20
-        })
-        .attr("y", function(d) {
-          return y(d.state_name) + y.bandwidth() / 2
-        })
-        .attr("text-anchor", "middle")
-        .text(function(d) {
-          return d.ranking
-        })
-        .style("fill", "white")
+        //adding values to bars
+        bar.selectAll(".text")
+          .data(data).enter()
+          .append("text")
+          .attr("class", "bar-text")
+          .attr("dy", ".35em")
+          .attr("x", function(d) {
+            return x(d.ranking) - 10
+          })
+          .attr("text-anchor", "middle")
+          .text(function(d) {
+            return d.ranking
+          })
+          .style("fill", "white")
+
+
+      if(geo_level == "state"){
+        y.domain(data.map(function(d) {
+          return d.state_name
+        })).padding(0.3);
+
+        bar.selectAll('.bar')
+          .attr('y', function(d) {
+            return y(d.state_name)
+          })
+          .attr("height", y.bandwidth()); //assigning hieght of bars
+
+          bar.selectAll(".bar-text")
+          .attr("y", function(d) {
+            return y(d.state_name) + y.bandwidth() / 2
+          })
+
+      } else if (geo_level == "county") {
+
+        y.domain(data.map(function(d) {
+          return d.county_name
+          })).padding(0.3);
+
+          bar.selectAll('.bar')
+            .attr('y', function(d) {
+              return y(d.county_name)
+            })
+            .attr("height", y.bandwidth()); //assigning hieght of bars
+
+            bar.selectAll(".bar-text")
+            .attr("y", function(d) {
+              return y(d.county_name) + y.bandwidth() / 2
+            })
+      } else if (geo_level == "venue") {
+        y.domain(data.map(function(d) {
+          return d.venue
+          })).padding(0.3);
+
+          bar.selectAll('.bar')
+            .attr('y', function(d) {
+              return y(d.venue)
+            })
+            .attr("height", y.bandwidth()); //assigning hieght of bars
+
+            bar.selectAll(".bar-text")
+            .attr("y", function(d) {
+              return y(d.venue) + y.bandwidth() / 2
+            })
+      }
+      //assigning y-axis
+      bar.append('g').attr("class", "y axis").call(d3.axisLeft(y));
+
     }
 
   });
