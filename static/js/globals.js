90e320b41a4d92fc18b6b144d54be0abd2e08cd0
richa-sud <richasud@gmail.com>
2019-05-29 15:23:15 +0100

    fixing sub genre text

--- static/js/globals.js
+++ static/js/globals.js
@@ -2,37 +2,37 @@ const GENRES = {
   "pop": {
     "color": "#002f81",
     "label": "pop",
-    "sub_genres": "pop punk, adult contemporary, electropop"
+    "sub_genres": "pop punk, adult contemporary and electropop"
   },
   "rock": {
     "color": "#2a7187",
     "label": "rock",
-    "sub_genres": "glam metal, soft rock, metal, alternative rock, indie rock, deathgrind"
+    "sub_genres": "glam metal, soft rock, metal, alternative rock, indie rock and deathgrind"
   },
   "hip_hop": {
     "color": "#0381b4",
     "label": "hip hop",
-    "sub_genres": "hardcore hip hop, conscious hip hop, old school hip hop, gangsta rap"
+    "sub_genres": "hardcore hip hop, conscious hip hop, old school hip hop and gangsta rap"
   },
   "rnb": {
     "color": "#4a2777",
     "label": "R&B",
-    "sub_genres": "neo soul, funk, soul, rhythm and blues, disco, post-disco"
+    "sub_genres": "neo soul, funk, soul, rhythm and blues, disco and post-disco"
   },
   "classical_and_jazz": {
     "color": "#b41162",
     "label": "classical & jazz",
-    "sub_genres": "classical, jazz blues, vocal jazz, gypsy jazz, smooth jazz"
+    "sub_genres": "classical, jazz blues, vocal jazz, gypsy jazz and smooth jazz"
   },
   "electronic": {
     "color": "#bf9076",
     "label": "electronic & dance",
-    "sub_genres":  "tribal house, indietronica, chillwave, eurodance"
+    "sub_genres":  "tribal house, indietronica, chillwave and eurodance"
   },
   "country_and_folk": {
     "color": "#56371b",
     "label": "country & folk",
-    "sub_genres": "ranchera, indie folk, country, progressive bluegrass"
+    "sub_genres": "ranchera, indie folk, country and progressive bluegrass"
   },
 };
 
